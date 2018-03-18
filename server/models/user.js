64e43e4e270668b4f2e4217c8b47d567db633898
Shawn Twenter <shawn.twenter@storage-mart.com>
2018-03-17 21:53:28 -0500

    Ad POST /todos route and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+// User
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
+});
+
+
+module.exports = {
+    User
+};
